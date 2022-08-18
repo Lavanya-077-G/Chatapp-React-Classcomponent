ba26058059ea48a131c6beca654bf67084e1d19c
Lavanya-077-G <111418903+Lavanya-077-G@users.noreply.github.com>
2022-08-18 11:23:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default class App extends Component {
   };
 
   render() {
-      
+    
     return (
       <div className="user">
         <Container val={this.person1} messages={this.state.person1Message}  func={this.messagearrupdate} />
